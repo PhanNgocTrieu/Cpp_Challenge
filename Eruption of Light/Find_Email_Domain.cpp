6e23454540da89d61acd21f69d9f6ca0e46f8fd4
PhanNgocTrieu <47733751+PhanNgocTrieu@users.noreply.github.com>
2020-10-23 16:33:07 +0700

    Create Find_Email_Domain.cpp

---
+++ Eruption of Light/Find_Email_Domain.cpp
@@ -0,0 +1,37 @@
+//An email address such as "John.Smith@example.com" is made up of a local part("John.Smith"),
+//an "@" symbol, then a domain part("example.com").
+//
+//The domain name part of an email address may only consist of letters, digits, hyphensand dots.The local part, however, also allows a lot of different special characters.Here you can look at several examples of correctand incorrect email addresses.
+//
+//Given a valid email address, find its domain part.
+//
+//Example
+//
+//For address = "prettyandsimple@example.com", the output should be
+//findEmailDomain(address) = "example.com";
+//For address = "fully-qualified-domain@codesignal.com", the output should be
+//findEmailDomain(address) = "codesignal.com".
+
+
+#include <iostream>
+#include <string>
+
+std::string findEmailDomain(std::string address) {
+	
+	int Pos = address.find_last_of("@") + 1;
+	int Len = address.length() - Pos;
+	char buffer[20];
+	std::size_t length = address.copy(buffer, Len, Pos);
+	buffer[length] = '\0';
+	std::cout << "buffer contains: " << buffer << '\n';
+	return buffer;
+}
+
+
+int main()
+{
+	std::string address = "Better@gmail.com";
+	findEmailDomain(address);
+
+	return 0;
+}
