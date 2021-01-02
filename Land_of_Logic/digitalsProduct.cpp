7ddaf583bf6291529f8923cc083accbe8dc74ad8
PhanNgocTrieu <47733751+PhanNgocTrieu@users.noreply.github.com>
2021-01-02 10:56:23 +0700

    Update digitalsProduct.cpp

--- Land_of_Logic/digitalsProduct.cpp
+++ Land_of_Logic/digitalsProduct.cpp
@@ -10,30 +10,26 @@ For product = 19, the output should be
 digitsProduct(product) = -1.
 */
 
-
 #include <iostream>
 #include <vector> 
 
-void Find(std::vector<int> &track, int product)
+void Find(std::vector<int> &track, int &product)
 {
     std::cout << "running this! \n";
 
     for (int i = 9; i > 1; i--)
     {
-        if (product % i == 0)
+        while (product % i == 0)
         {
-            std::cout << "I was chosen: " << i << "\n";
             track.push_back(i);
-            std::cout << "Already push " << i << " into track! \n";
+            std::cout << "i" << i << "\n";
             product = product / i;
-            Find(track,product);
-            break;
+            std::cout << "product " << product << '\n'; 
         }
     }
 }
 
 int digitsProduct(int product) {   
-    
     std::vector<int> track;
     int res = 0;
     if (product == 0)
@@ -61,7 +57,15 @@ int digitsProduct(int product) {
             res = res*10;
             }
         res = res / 10;
-        return res;
+
+        if (product != 1)
+        {
+            return -1;
+        }
+        else
+        {
+            return res;
+        }
     }
         
 }
@@ -70,6 +74,6 @@ int digitsProduct(int product) {
 int main()
 {
    // std::cout << digitalNumber(2312) << "\n";
-    std::cout << digitsProduct(1) << "\n";
+    std::cout << digitsProduct(484) << "\n";
     return 0;
 }
