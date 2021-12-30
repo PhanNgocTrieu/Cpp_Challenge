1ab3a5a0775d3709da12332178b0690d08c0a2f3
PhanNgocTrieu <bettertogetheronit@gmail.com>
2021-12-31 02:38:19 +0700

    data structure problems

--- DataStructure/include/dataStructure.h
+++ DataStructure/include/dataStructure.h
@@ -6,20 +6,49 @@ using namespace std;
 
 namespace DTST
 {
+    template <class _Typ>
+    struct SinglyLinkedListNode
+    {
+        // value with type of _Typ
+        _Typ m_value;
+
+        // pointer to the next node
+        SinglyLinkedListNode* next;
+
+        public: 
+            SinglyLinkedListNode() {};
+            SinglyLinkedListNode(const _Typ& value) : m_value{value}, next{nullptr}
+            {}
+            ~SinglyLinkedListNode() {};
+            
+            void push_frontIntValue(const _Typ& _value) 
+            {
+                SinglyLinkedListNode* newNode = SinglyLinkedListNode(_value)
+                if (this == nullptr)
+                {
+                    this = newNode;
+                    return;
+                }
+
+                newNode->next = this;
+                this = newNode;
+            }
+
+            void print()
+            {
+                SinglyLinkedListNode* iRun = this;
+
+                while (iRun != nullptr && iRun->next != nullptr)
+                {
+                    cout << iRun->m_value << " -> ";
+                }
+                cout << endl;
+            }
+    };
+
     class LinkedListProblems
     {
-        private:
-            struct SinglyLinkedListNode
-            {
-                int m_intValue;
-                float m_floatValue;
-                double m_doubleValue;
-                string m_stringValue;
-                uint16_t m_uint16Value;
-                uint32_t m_uint32Value;
-                SinglyLinkedListNode* nextNode;
-            };
-            
+                   
         public:
             /**
              * @brief Construct a new Linked List Problems object
@@ -42,15 +71,39 @@ namespace DTST
              * 
              * @param list1 first linked list
              * @param list2 second linked list
+             * 
+             * @return return the resulting of 2 lists
              */
-            bool compare_lists(SinglyLinkedListNode* list1, SinglyLinkedListNode* list2);
+            bool compare_lists(SinglyLinkedListNode<int>* list1, SinglyLinkedListNode<int>* list2);
 
+            /**
+             * @brief Giving a list -> reversing the given list
+             * 
+             * @param head the pointer to the list
+             * @return SinglyLinkedListNode* return the reversed list
+             */
+            SinglyLinkedListNode<int>* reverse(SinglyLinkedListNode<int>* head);
+
+            /**
+             * @brief   
+             * Given pointers to the heads of two sorted linked lists, merge them into a single, sorted linked list. 
+             * Either head pointer may be null meaning that the corresponding list is empty.
+             * 
+             * @param head1 pointer to the first list
+             * @param head2 pointer to the second list
+             * @return SinglyLinkedListNode<int>* return the merged list from 2 lists
+             */
+            SinglyLinkedListNode<int>* mergeLists(SinglyLinkedListNode<int>* head1, SinglyLinkedListNode<int>* head2);
 
         private:
             void printLinkedList();
 
         private:
-            SinglyLinkedListNode* m_linkedList;
+            SinglyLinkedListNode<int>* m_intLinkedList;
+            SinglyLinkedListNode<float>* m_floatLinkedList;
+            SinglyLinkedListNode<double>* m_doubleLinkedList;
+            SinglyLinkedListNode<string>* m_stringLinkedList;
+            
     };
 
 
--- DataStructure/src/dataStructure.cpp
+++ DataStructure/src/dataStructure.cpp
@@ -1,1 +1,113 @@
 #include "dataStructure.h"
+
+
+namespace DTST
+{
+    LinkedListProblems::LinkedListProblems()
+    {}
+
+    LinkedListProblems::~LinkedListProblems()
+    {}
+
+
+    /**
+     * @brief solving compare list
+     *      * 
+     * @remark already test -> successful
+     */
+    bool LinkedListProblems::compare_lists(SinglyLinkedListNode<int>* list1, SinglyLinkedListNode<int>* list2)
+    {
+        if (list1 == nullptr && list2 == nullptr)
+            return 1;
+
+        bool isValueEqual = true;
+
+        while (list1 != nullptr && list2 != nullptr)
+        {
+
+            if (list1->m_value != list2->m_value)
+            {
+                isValueEqual = false;
+                return 0;
+            }
+            list1 = list1->next;
+            list2 = list2->next;              
+     
+        }
+        
+        if (list1 == nullptr && list2 == nullptr)
+        {    
+            if (isValueEqual)
+                return true;
+            else
+                return false;
+        }
+        return 0; 
+    }
+
+    /**
+     * @brief solving reversing list
+     * 
+     * @remark already test -> successful
+     */
+    SinglyLinkedListNode<int>* LinkedListProblems::reverse(SinglyLinkedListNode<int>* head)
+    {
+        SinglyLinkedListNode<int>* pre = nullptr;
+        SinglyLinkedListNode<int>* curr = head;
+        SinglyLinkedListNode<int>* next = nullptr;
+        
+        
+        while (curr != NULL) {
+            next = curr -> next;
+            curr -> next = pre;
+            pre = curr;
+            curr = next;
+        }
+        
+        return pre;
+    }
+
+    /**
+     * @brief 
+     * 
+     * @remark 
+     */
+    SinglyLinkedListNode<int>* LinkedListProblems::mergeLists(SinglyLinkedListNode<int>* head1, SinglyLinkedListNode<int>* head2)
+    {
+        
+    }
+
+
+
+    /**
+     * @brief printing the list of the method
+     * 
+     */
+    void LinkedListProblems::printLinkedList()
+    {
+        // print the list of integer numbers
+        if (m_intLinkedList)
+        {
+            m_intLinkedList->print();
+        }
+        
+        // print the list of float numbers
+        if (m_floatLinkedList)
+        {
+            m_floatLinkedList->print();
+        }
+
+        // print the list of double numbers
+        if (m_doubleLinkedList)
+        {
+            m_doubleLinkedList->print();
+        }
+
+        // print the list of strings
+        if (m_stringLinkedList)
+        {
+            m_stringLinkedList->print();
+        }
+    }
+
+};
