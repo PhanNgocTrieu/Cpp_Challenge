ed299918a66d5dcf132db203148467f466dcbbde
PhanNgocTrieu <bettertogetheronit@gmail.com>
2022-01-04 18:16:32 +0700

    Updated again

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/trieujohn/Documents/DoingWithGit/Cpp_Challenge
-# Build directory: /home/trieujohn/Documents/DoingWithGit/Cpp_Challenge/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
